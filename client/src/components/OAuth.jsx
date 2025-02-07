4b99e7b8436c10ee512a29f428472716d0ba28fc
Sohail Batoor <sohailshahidi1435@gmail.com>
2025-02-07 10:45:50 +0800

    Add OAuth.jsx file

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,7 @@
+const OAuth = () => {
+  return (
+    <button className="bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95">Continue with google</button>
+  )
+}
+
+export default OAuth
