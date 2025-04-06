b4448e8151e601ecc483a58431adb36025988b29
Sohail Batoor <sohailshahidi1435@gmail.com>
2025-04-06 18:38:16 +0800

    Add user profilePicture to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,10 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  console.log(currentUser);
+
   return (
     <section className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -15,7 +19,11 @@ const Header = () => {
             <li>About</li>
           </Link>
           <Link to='/sign-in'>
-            <li>Sign In</li>
+            {currentUser ? (
+              <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' referrerPolicy="no-referrer" />
+            ) : (
+              <li>Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
