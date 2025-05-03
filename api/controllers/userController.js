e77ba2b8dacc13736a027b171f64451c9bb7aac9
Sohail Batoor <sohailshahidi1435@gmail.com>
2025-05-03 22:30:05 +0800

    Add deleteUser route and controller

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -36,3 +36,17 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+// delete user
+
+export const deleteUser = async () => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, 'You can only delete your account!'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json('User has been deleted...');
+  } catch (error) {
+    next(error);
+  }
+};
